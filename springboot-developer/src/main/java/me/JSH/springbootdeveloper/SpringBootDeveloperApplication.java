7079b38eaed643d3eb28527205f8f891ebd4014b
starlit-sound <starhyeun@gmail.com>
2024-03-10 16:12:27 +0900

    f

--- springboot-developer/src/main/java/me/JSH/springbootdeveloper/SpringBootDeveloperApplication.java
+++ springboot-developer/src/main/java/me/JSH/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,10 +3,11 @@ package me.JSH.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//fsdf
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
